ccbcc4c31a5bff0cd17e9c940e4d8f96322c7436
maunilswadas <swadasmaunil@gmail.com>
2017-03-04 19:50:40 +0530

    update: pop heads set

--- js/events.js
+++ js/events.js
@@ -2,6 +2,6 @@ var JQDoc = $(document);
 JQDoc.ready(function () {
     //begin:background color and img
     $('body').css('background-color', $('#ubg').data('ubgc'));
-    $('.udaan-pop-heads').css('background-image', "url('img/" + $('#ubg').data('ubgi') + "')");
+    $('.udaan-pop-heads').attr('src',"img/" + $('#ubg').data('ubgi'));
     //end: background color and img
 });
