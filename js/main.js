f2500c39e9b2247bc79b2cce00ab3a623eedd794
maunilswadas <swadasmaunil@gmail.com>
2017-03-05 16:57:17 +0530

    add: udaan logo set, tech background set

--- js/main.js
+++ js/main.js
@@ -2,12 +2,22 @@ var JQDoc = $(document);
 JQDoc.ready(function () {
     //begin: scrolling
     $(".main").onepage_scroll({
-        animationTime: 1000,
+        animationTime: 600,
         easing: 'ease-in-out',
         updateURL: true,
         loop: false,
         pagination: false,
         direction: "horizontal"
+    });
+    $(document).keydown(function (e) {
+        if (e.keyCode == 37) {
+            $(".main").moveUp();
+            return false;
+        }
+        if (e.keyCode == 39) {
+            $(".main").moveDown();
+            return false;
+        }
     });
     //end : scrolling
 
@@ -22,8 +32,4 @@ JQDoc.ready(function () {
         $(".main").moveUp();
     });
     //end: hammer swipe events
-
-    //begin:collagePlus
-    $('.Collage').collagePlus();
-    //end : collagePlus
 });
