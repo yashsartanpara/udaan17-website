8db1e97970a5481ae9f65ee2e10993aa05d745b3
maunilswadas <swadasmaunil@gmail.com>
2017-03-05 19:01:09 +0530

    add:new logo in titlepage , background city line added, js for modals and new modals added

--- js/events.js
+++ js/events.js
@@ -5,23 +5,12 @@ JQDoc.ready(function () {
     $('body').css('background-color', $('#ubg').data('ubgc'));
     $('.udaan-pop-heads').attr('src', "img/" + $('#ubg').data('ubgi'));
     //end: background color and img
-    var color = {
-    	ironman:["216,79,63","121,44,36","193,153,81"]
-    }
-    window.color = color;
-    // var figure = $('#ubg').data('ubgi').slice(0,-4);
-    // var acolor = color[figure][Math.floor(Math.random()*color[figure].length)];
-	// window.acolor = acolor;
-	// console.log(acolor);
-	// document.on('click','')
-
-
-
 });
-	function openeventmodal()
+	function openeventmodal(elem)
 	{
-		var temp = document.getElementById("events-modal");
-		$(temp).removeClass("hidden");
+	    console.log($(elem).children('a').attr('href'));
+		var temp = document.getElementById($(elem).children('a').attr('href').slice(1));
+		$(temp).css('display','block');
 
 	}
 
