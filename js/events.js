a488a679e3b50a1440ba0c2513b37083b3ab011d
Rohit <rvprvprvp7824@gmail.com>
2017-03-05 17:02:39 +0530

    Changes in Modal

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 var JQDoc = $(document);
 JQDoc.ready(function () {
     //begin:background color and img
+   
     $('body').css('background-color', $('#ubg').data('ubgc'));
     $('.udaan-pop-heads').attr('src', "img/" + $('#ubg').data('ubgi'));
     //end: background color and img
+    var color = {
+    	ironman:["216,79,63","121,44,36","193,153,81"]
+    }
+    window.color = color;
+    // var figure = $('#ubg').data('ubgi').slice(0,-4);
+    // var acolor = color[figure][Math.floor(Math.random()*color[figure].length)];
+	// window.acolor = acolor;
+	// console.log(acolor);
+	// document.on('click','')
+
+
+
 });
+	function openeventmodal()
+	{
+		var temp = document.getElementById("events-modal");
+		$(temp).removeClass("hidden");
+
+	}
+
+	function closeeventmodal()
+	{
+		var temp = document.getElementById("events-modal");
+		$(temp).addClass("hidden");
+	}
+
