aec9b13f0cf51674419224b3b4e3a57ebd75689a
Chintan Acharya <acharya.chintan96@gmail.com>
2017-03-08 18:40:08 +0530

    add: modal scripts

--- js/events.js
+++ js/events.js
@@ -1,20 +1,84 @@
 var JQDoc = $(document);
+
+Handlebars.registerHelper('succ', function (index) {
+    return index + 1
+});
+
+Handlebars.registerHelper('urlencode', function (str) {
+    return encodeURIComponent(str);
+});
+
 JQDoc.ready(function () {
     //begin:background color and img
 
     $('body').css('background-color', $('#ubg').data('ubgc'));
-    $('.udaan-pop-heads').attr('src', "img/" + $('#ubg').data('ubgi')+".svg");
-    $('.pop-head-line').attr('src', "img/" + $('#ubg').data('ubgi')+"-line.svg");
+    $('.udaan-pop-heads').attr('src', "img/" + $('#ubg').data('ubgi') + ".svg");
+    $('.pop-head-line').attr('src', "img/" + $('#ubg').data('ubgi') + "-line.svg");
     //end: background color and img
+    //begin: jqcloud
+    document.eventData = getTechEvents($('#ubg').data('udpt'));
+    console.log(getWordArray(document.eventData));
+    $('#event').html(getWordArray(document.eventData).map(function (event) {
+        var randomPadding = Math.floor(Math.random() * 25) + 'px';
+        return '<li style="padding: 0px ' +
+            randomPadding  + '"' +
+            '" class="event-name" data-id="' +
+            event.id  + '">' +
+            event.text +
+            '</li>'
+    }));
 
-    $('.modal-close').on('click',function () {
-        $('.modal').css('display', 'none');
+    $('.event-name').click(function(){
+        openeventmodal(this);
     });
-});
-function openeventmodal(elem) {
-    console.log($(elem).children('a').attr('href'));
-    var temp = document.getElementById($(elem).children('a').attr('href').slice(1));
-    $(temp).css('animation', 'zoomIn 0.4');
-    $(temp).css('display', 'block');
 
+    // end:jqcloud
+    //start: modal
+    //end:modal
+});
+window.onhashchange = function () {
+    if (location.hash == '') {
+        closeEventModal();
+    }
+};
+
+function openeventmodal(elem) {
+    var index = +($(elem).data('id'));
+    window.location.hash = 'event' + index;
+    var modal = $('#event-modal');
+    var modalData = (document.eventData[index]);
+    modalData.theme = theme;
+    modal.html(Handlebars.templates['event'](modalData));
+    $('.close').on('click', closeEventModal);
+    modal.css('animation', 'zoomIn 0.4');
+    modal.css('display', 'block');
+
+}
+
+function closeEventModal() {
+    $('#event-modal').css('display', 'none');
+}
+
+var mouseoverhandler = function () {
+    $(this).addClass('glow-blue')
+        .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $(this).removeClass('glow-blue');
+        });
+};
+
+function getTechEvents(deptName) {
+    return data.tech.find(function (dept) {
+        return dept.name == deptName;
+    }).events;
+}
+
+function getWordArray(events) {
+    return events.map(getWordData);
+}
+
+function getWordData(event, id) {
+    return {
+        text: event.name,
+        id: id
+    }
 }
