90ba43a653e377f3b25487f5ade7f97765e86ca1
Dixita Ganatra <dixita0607@gmail.com>
2017-03-09 15:19:56 +0530

    Delete unused images Improve handlebars scripts Create cultural events and nights page

--- js/events.js
+++ js/events.js
@@ -1,5 +1,3 @@
-var JQDoc = $(document);
-
 Handlebars.registerHelper('succ', function (index) {
     return index + 1
 });
@@ -8,70 +6,46 @@ Handlebars.registerHelper('urlencode', function (str) {
     return encodeURIComponent(str);
 });
 
-JQDoc.ready(function () {
-    //begin:background color and img
+var events = data.tech.find(function (department) {
+    return department.alias == window.department
+}).events;
 
-    $('body').css('background-color', $('#ubg').data('ubgc'));
-    $('.udaan-pop-heads').attr('src', "img/" + $('#ubg').data('ubgi') + ".svg");
-    $('.pop-head-line').attr('src', "img/" + $('#ubg').data('ubgi') + "-line.svg");
-    //end: background color and img
-    //begin: jqcloud
-    document.eventData = getTechEvents($('#ubg').data('udpt'));
-    $('#event').html(getWordArray(document.eventData).map(function (event) {
-        var randomPadding = Math.floor(Math.random() * 25) + 'px';
-        return '<li style="padding: 0px ' +
-            randomPadding  + '"' +
-            '" class="event-name" data-id="' +
-            event.id  + '">' +
-            event.text +
-            '</li>'
+$(function () {
+    $('#events').html(events.map(function (event, index) {
+        return '<li style="padding: 0 ' + Math.floor(Math.random() * 25) + 'px;"' +
+            '" class="event-name" data-index="' + index + '">' + event.name + '</li>'
     }));
-
-    $('.event-name').click(function(){
-        openeventmodal(this);
+    $('.event-name').click(function () {
+        openEventModal(this);
     });
-
-    // end:jqcloud
-    //start: modal
-    //end:modal
 });
+
 window.onhashchange = function () {
-    if (location.hash == '') {
-        closeEventModal();
-    }
+    if (location.hash == '') closeEventModal();
+    else populateModal(location.hash.substr(7))
 };
 
-function openeventmodal(elem) {
-    var index = +($(elem).data('id'));
-    window.location.hash = 'event' + index;
-    var modal = $('#event-modal');
-    var modalData = (document.eventData[index]);
-    modalData.theme = theme;
-    modal.html(Handlebars.templates['event'](modalData));
-    $('.close').on('click', closeEventModal);
-    modal.css('animation', 'zoomIn 0.4');
-    modal.css('display', 'block');
-
+function openEventModal(elem) {
+    var index = $(elem).data('index');
+    window.location.hash = 'event-' + index;
 }
 
 function closeEventModal() {
-    $('#event-modal').css('display', 'none');
+    var modal = $('#event-modal')
+        .removeClass('modal-open-animation')
+        .addClass('modal-close-animation')
+    setTimeout(function () {
+        modal.css('display', 'none');
+    }, 500)
 }
 
-
-function getTechEvents(deptName) {
-    return data.tech.find(function (dept) {
-        return dept.name == deptName;
-    }).events;
-}
-
-function getWordArray(events) {
-    return events.map(getWordData);
-}
-
-function getWordData(event, id) {
-    return {
-        text: event.name,
-        id: id
-    }
+function populateModal(index) {
+    $('#event-modal')
+        .html(Handlebars.templates['event'](events[index]))
+        .removeClass('modal-close-animation')
+        .css('display', 'block')
+        .addClass('modal-open-animation');
+    $('.close').on('click', function () {
+        history.back()
+    });
 }
