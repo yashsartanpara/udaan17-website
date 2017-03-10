952de56137f84e8d651b8db498083ddceca53761
Dixita Ganatra <dixita0607@gmail.com>
2017-03-10 13:38:31 +0530

    Add content to non-tech, cultural and nights pages, Add pop-heads-line graphics, Update script for events

--- js/events.js
+++ js/events.js
@@ -1,51 +1,50 @@
 Handlebars.registerHelper('succ', function (index) {
-    return index + 1
+  return index + 1
 });
 
 Handlebars.registerHelper('urlencode', function (str) {
-    return encodeURIComponent(str);
+  return encodeURIComponent(str);
 });
 
-var events = data.tech.find(function (department) {
-    return department.alias == window.department
-}).events;
-
 $(function () {
-    $('#events').html(events.map(function (event, index) {
-        return '<li style="padding: 0 ' + Math.floor(Math.random() * 25) + 'px;"' +
-            '" class="event-name" data-index="' + index + '">' + event.name + '</li>'
-    }));
-    $('.event-name').click(function () {
-        openEventModal(this);
-    });
+  $('#nontechEvents').html(events.map(function (event, index) {
+    return '<div class="col-xs-4 col-xs-offset-2 end-xs event-name" data-index="' + index + '">' + event.name + '</div>'
+  }));
+  $('#events').html(events.map(function (event, index) {
+    return '<li style="padding: 0 ' + Math.floor(Math.random() * 25) + 'px;"' +
+      '" class="event-name" data-index="' + index + '">' + event.name + '</li>'
+  }));
+  $('.event-name').click(function () {
+    openEventModal(this);
+  });
 });
 
 window.onhashchange = function () {
-    if (location.hash == '') closeEventModal();
-    else populateModal(location.hash.substr(7))
+  if (location.hash == '') closeEventModal();
+  else populateModal(location.hash.substr(7))
 };
 
 function openEventModal(elem) {
-    var index = $(elem).data('index');
-    window.location.hash = 'event-' + index;
+  var index = $(elem).data('index');
+  window.location.hash = 'event-' + index;
 }
 
 function closeEventModal() {
-    var modal = $('#event-modal')
-        .removeClass('modal-open-animation')
-        .addClass('modal-close-animation')
-    setTimeout(function () {
-        modal.css('display', 'none');
-    }, 500)
+  var modal = $('#event-modal')
+    .removeClass('modal-open-animation')
+    .addClass('modal-close-animation')
+  setTimeout(function () {
+    modal.css('display', 'none');
+  }, 500)
 }
 
 function populateModal(index) {
-    $('#event-modal')
-        .html(Handlebars.templates['event'](events[index]))
-        .removeClass('modal-close-animation')
-        .css('display', 'block')
-        .addClass('modal-open-animation');
-    $('.close').on('click', function () {
-        history.back()
-    });
+  $('#event-modal')
+    .html(Handlebars.templates['event'](events[index]))
+    .removeClass('modal-close-animation')
+    .css('display', 'block')
+    .addClass('modal-open-animation');
+  $('.close').on('click', function () {
+    history.back()
+  });
 }
